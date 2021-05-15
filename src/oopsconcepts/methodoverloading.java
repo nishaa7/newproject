a79c6f2d6fe8f35489c6b2dba4a0980a1a146ba9
nishaa7 <nisha.agrawal28@gmail.com>
2021-05-15 04:11:36 +0000

    core java

---
+++ src/oopsconcepts/methodoverloading.java
@@ -0,0 +1,27 @@
+package oopsconcepts;
+
+public class methodoverloading {
+
+	//public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		public void add(int a , int b)
+		{
+			int sum = a+b;
+			System.out.println("Integer argument " +sum);
+		}
+		public void add(double a , double b)
+		{
+			double sum = a+b;
+			System.out.println("Integer argument " +sum);
+		}
+		public void add(double a , double b, double c)
+		{
+			double sum = a+b+c;
+			System.out.println("Integer argument " +sum);
+		}
+		}
+
+	
+
+
--- src/oopsconcepts/runnerclass.java
+++ src/oopsconcepts/runnerclass.java
@@ -5,7 +5,7 @@ public class runnerclass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		classA Emp1 = new classA();
+		/*classA Emp1 = new classA();
 		Emp1.Display();
 		classA Emp2 = new classA("nisha","xyz",1000);
 
@@ -16,7 +16,13 @@ public class runnerclass {
 		obj1.Display();
 		obj1.show();
 		classC obj2 = new classC();
-		obj2.run();
+		obj2.run();*/
+		
+		methodoverloading obj3 = new methodoverloading();
+		obj3.add(3, 5);
+		obj3.add(4.6, 5.5);
+		obj3.add(3.3, 5.5, 7.7);
+		
 	}
 
 }
